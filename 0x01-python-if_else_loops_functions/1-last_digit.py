0ed4c23901c6e8f5dee6652840fddeaebfc03309
betty irungu <bettyirungu15@gmail.com>
2024-01-02 05:53:51 -0800

    variable number will store a different value every time you will run this program

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+lastdig = abs(number) % 10
+if number < 0:
+    lastdig = -(lastdig)
+str_val = "Last digit of {}".format(number, lastdig)
+if lastdig > 5:
+    print(f"{str_val} and is greater than 5")
+elif lastdig == 0:
+    print(f"{str_val} and is 0")
+else:
+    print(f"{str_val} and is less than 6 and not 0")
