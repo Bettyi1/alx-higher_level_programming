8e90e89da4d0b1ef9f47ed5d4fdd8fced7f2de6a
betty irungu <bettyirungu15@gmail.com>
2024-01-02 18:50:24 -0800

    last digit

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    lastdig = number % 10
     if number < 0:
         number = -number
+        lastdig = number % 10
         print("{}".format(lastdig), end='')
         return (lastdig)
