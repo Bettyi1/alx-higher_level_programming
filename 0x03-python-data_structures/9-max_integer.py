30f1eb18ce8d7aea24a0c648a0158c13bfcfea38
betty irungu <bettyirungu15@gmail.com>
2024-01-08 06:53:55 -0800

    max interger

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -4,8 +4,7 @@ def max_integer(my_list=[]):
         return (None)
     else:
         max_value = my_list[0]
-
         for i in range (len(my_list)):
             if my_list[i] > max_value:
                 max_value = my_list[i]
-                return (max_value)
+        return (max_value)
