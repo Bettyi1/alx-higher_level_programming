7bdd4633e6b043118542f25a414237652e72796e
betty irungu <bettyirungu15@gmail.com>
2024-01-10 01:11:25 -0800

    prints the number of and the list of its arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     elif j == 1:
         print("{} argument:".format(j))
     else:
-        print("{} argyments:".format(j))
-        for i in range (j):
+        print("{} arguments:".format(j))
+    for i in range (j):
             print("{}: {:s}".format(i + 1, argv[i + 1]))
