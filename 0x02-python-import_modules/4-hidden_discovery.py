17d882195db33446139f5d0b8a066718163d054d
betty irungu <bettyirungu15@gmail.com>
2024-01-09 22:28:00 -0800

    prints names

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 if __name__ == "__main__":
     from hidden_4 import *
-    functs = dir()
-    for i in range (0, len(functs))
-    if functs[i][:2] != "__":
-        print("{:s}".format(functs[i]))
+    funcs = dir()
+    for i in range(0, len(funcs)):
+        if funcs[i][:d] != "__":
+            print("{:s}".format(funcs[i]))
