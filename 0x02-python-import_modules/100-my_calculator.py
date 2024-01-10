4e76bc38c744a4cf64076a2bc876b39694dbf093
betty irungu <bettyirungu15@gmail.com>
2024-01-10 00:09:01 -0800

    program that imports all functions from the file

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    from sys import argv
+    count_args = len(argv)
+
+    if count_args != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit (1)
+    a = int(arg[1])
+    operator = argv([2])
+    b = argv([3])
+    if operator == '+':
+        print(f"{a} {operator} {a} = {add(a, b)}")
+    elif operator == '-':
+        print(f"{a} {operator} {a} = {sub(a, b)}")
+    elif operator == '*':
+        print(f"{a} {operator} {a} = {mul(a, b)}")
+    elif operator == '/':
+        print(f"{a} {operator} {a} = {div(a, b)}")
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
